8aa990b47e67460268dff3298fcb3e58fcc8593a
Cyril Mougel <cyril.mougel@gmail.com>
2012-04-24 10:02:15 +0200

    add configuration about blog

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/shingara-blog-en/"
+http_images_path = "/shingara-blog-en/images"
+http_fonts_path = "/shingara-blog-en/fonts"
+css_dir = "public/shingara-blog-en/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
